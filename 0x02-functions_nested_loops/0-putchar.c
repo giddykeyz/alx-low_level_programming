90a787941ac68378f370024c2f264fa1073cd1e8
giddykeyz <bardigideon@gmail.com>
2022-06-07 05:08:50 -0700

    use putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Bardi Gideon
+ *
+ */
+
+#include "putchar.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+  	_putchar('\n');
+
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readme for 0x02 c project

026e3820f3d0b97c48c84666123cde3964ed46cd
giddykeyz <bardigideon@gmail.com>
2022-06-13 12:51:48 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,22 +1,17 @@
-/*
- * File: 2-strlen.c
- * Auth: Bardi Gideon
- */
-
 #include "main.h"
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @str: string.
  *
- * Return: The length of @str.
+ * Return: Length.
  */
 size_t _strlen(const char *str)
 {
-	size_t length = 0;
+	size_t len = 0;
 
 	while (*str++)
-		length++;
+		len++;
 
-	return (length);
+	return (len);
 }

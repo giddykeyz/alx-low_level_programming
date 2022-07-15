2cb6fecf4e3670d59332d8318f5451bd8dda5b24
giddykeyz <bardigideon@gmail.com>
2022-07-14 20:07:30 -0700

    fixed counter

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
   
  size_t print_list(const list_t *h) 
  { 
-         size_t nelem; 
+         size_t counter; 
   
-         nelem = 0; 
+         counter = 0; 
          while (h != NULL) 
          { 
                  if (h->str == NULL) 
@@ -17,7 +17,7 @@
                  else 
                          printf("[%d] %s\n", h->len, h->str); 
                  h = h->next; 
-                 nelem++; 
+                 counter++; 
          } 
-         return (nelem); 
+         return (counter); 
  }

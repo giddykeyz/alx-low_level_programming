7bfa9e99b137b17ec0407ccd101324ee89391d8c
giddykeyz <bardigideon@gmail.com>
2022-09-22 11:35:10 -0700

    solution to task

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -4,7 +4,7 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
@@ -13,19 +13,14 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
+    hash_table_print(ht);
     hash_table_set(ht, "c", "fun");
-    hash_table_set(ht, "Jennie", "and Jay love asm");
-    hash_table_set(ht, "N", "kings");
-    hash_table_set(ht, "serafins", "Obelix");
-    hash_table_set(ht, "serafins", "Tuna fish?");
-    hash_table_set(ht, "depravement", "Holberton");
-    hash_table_set(ht, "serafins", "Bowling pins?");
-    hash_table_set(ht, "depravement", "is concrete");
-    hash_table_set(ht, "serafins", "Magic Djinns?");
-    hash_table_set(ht, "serafins", "Mystery");
-    hash_table_set(ht, "Betty", "Holberton");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
     hash_table_set(ht, "98", "Battery Street");
-	hash_table_print(ht);
-    hash_table_delete(ht);
+    hash_table_print(ht);
     return (EXIT_SUCCESS);
 }

50193cfb8b5f76f206c307750ae7af677f4fabb6
giddykeyz <bardigideon@gmail.com>
2022-11-10 11:31:51 -0800

    fixed code

--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
         [0, 1, 0, 0, 0, 0],
         [0, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0]
-     ]
-print(island_perimeter(grid))
+    ]
+    print(island_perimeter(grid))
+
